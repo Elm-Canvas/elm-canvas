3ae5fc9aa415f222c7fea17bf237c7923cfe9eb2
Chadtech <chadstearns1@gmail.com>
2016-12-16 22:47:35 -0500

    Canvas module now contains a function called canvas, which takes canvas data (List Int) and returns a canvas. Codes messy and isnt fast, but it works

--- src/elm/View.elm
+++ src/elm/View.elm
@@ -5,7 +5,7 @@ import Html.Attributes  exposing (..)
 import Html.Events      exposing (..)
 import Types            exposing (..)
 import Components       exposing (..)
-import List             exposing (repeat, map)
+import List             exposing (repeat, map, concat)
 import Array            exposing (Array, toIndexedList)
 import Canvas
 
@@ -14,10 +14,13 @@ view : Model -> Html Msg
 view model =
   div
   [ class "root" ]
-  [ ignorablePoint "Elm Canvas!" 
-  , clickablePoint "Add Canvas" AddCanvas
+  [ div 
+    [ style [ ("display", "inline-block") ] ] 
+    [ ignorablePoint "Elm Canvas!" 
+    , clickablePoint "Add Canvas" AddCanvas
+    ]
   , div 
-    []
+    [ ]
     (canvases model.canvases)
   ]
 
@@ -31,22 +34,68 @@ canvases canvases' =
 
 canvasView : (Int, Canvas) -> Html Msg
 canvasView (index, cv) =
-  let {width, height, data} = cv in
+  let 
+    {width, height, numberOfViews} = cv 
+    data = getData cv
+  in
   div 
   [ class "canvas-container"
   , style [ ("width", (toString width)) ] 
   ]
-  [ clickablePoint 
-      "Increase Width" 
-      (IncreaseWidth index)
-  , clickablePoint
-      "Decrease WIdth"
-      (DecreaseWidth index)
-  , Canvas.canvas 
-      [ id (toString index) ] 
-      width
-      height 
-      data
+  <|concat
+  [ map optionRow
+    [ [ point "Width" 
+      , clickablePoint "+" (IncreaseWidth index)
+      , clickablePoint "-" (DecreaseWidth index)
+      ]
+    , [ point "Height" 
+      , clickablePoint "+" (IncreaseHeight index)
+      , clickablePoint "-" (DecreaseHeight index)
+      ]
+    , [ point "Views"
+      , clickablePoint "+" (AddView index)
+      , clickablePoint "-" (RemoveView index)
+      ]
+    , [ point "Red" 
+      , clickablePoint "+" (IncreaseRed index)
+      , clickablePoint "-" (DecreaseRed index)
+      ]
+    , [ point "Green" 
+      , clickablePoint "+" (IncreaseGreen index)
+      , clickablePoint "-" (DecreaseGreen index)
+      ]
+    , [ point "Blue" 
+      , clickablePoint "+" (IncreaseBlue index)
+      , clickablePoint "-" (DecreaseBlue index)
+      ]
+    , [ point "Gradient"
+      , clickablePoint "Switch" (SwitchGradient index)
+      ]
+    ]
+  , repeat numberOfViews
+    <|Canvas.canvas 
+        [ id (toString index) ] 
+        width
+        height 
+        data
   ]
+
+getData : Canvas -> List Int
+getData {width, height, color, gradient} =
+  let 
+    pixels =
+      let 
+        (r, g, b) = color
+        dataSize  = width * height 
+      in
+      if gradient then
+        map 
+          (\a -> [ r, g, b, a % 256 ])
+          [ 0 .. dataSize ]
+      else
+        repeat dataSize [ r, g, b, 255]
+  in
+  concat pixels
+
 
 
