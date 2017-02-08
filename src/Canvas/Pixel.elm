f08c28b8b9a9c81a7beafc6ce7094c4ff862ae40
Chadtech <chadstearns1@gmail.com>
2017-02-07 23:44:36 -0500

    Applied elm format to everything

--- src/Canvas/Pixel.elm
+++ src/Canvas/Pixel.elm
@@ -1,5 +1,5 @@
 module Canvas.Pixel
-    exposing 
+    exposing
         ( put
         , putMany
         , line
@@ -18,27 +18,27 @@ type alias Position =
 
 
 toPoint : Position -> Point
-toPoint {x, y} =
+toPoint { x, y } =
     Point (toFloat x) (toFloat y)
 
 
 toPosition : Point -> Position
-toPosition {x, y} =
+toPosition { x, y } =
     Position (round x) (round y)
 
 
 put : Color -> Position -> List DrawOp
 put color position =
-    List.singleton <| putHelp (color, position)
+    List.singleton <| putHelp ( color, position )
 
 
-putMany : List (Color, Position) -> List DrawOp
+putMany : List ( Color, Position ) -> List DrawOp
 putMany =
     List.map putHelp
 
 
-putHelp : (Color, Position) -> DrawOp
-putHelp (color, position) =
+putHelp : ( Color, Position ) -> DrawOp
+putHelp ( color, position ) =
     PutImageData
         (fromColor color)
         (Size 1 1)
@@ -48,73 +48,88 @@ putHelp (color, position) =
 fromColor : Color -> Array Int
 fromColor color =
     let
-        {red, green, blue, alpha} =
+        { red, green, blue, alpha } =
             Color.toRgb color
     in
         Array.fromList
             [ red
             , green
             , blue
-            , round (alpha * 255) 
+            , round (alpha * 255)
             ]
+
 
 
 -- Brensenham Line Algorithm
 
 
-type alias BresenhamStatics = 
-  { finish : Position
-  , sx : Int
-  , sy : Int
-  , dx : Float
-  , dy : Float 
-  }
+type alias LineStatics =
+    { fx : Int      -- Finish 
+    , fy : Int
+    , sx : Int      -- Step
+    , sy : Int
+    , dx : Float    -- Change
+    , dy : Float
+    }
 
 
 line : Color -> Position -> Position -> List DrawOp
 line color p q =
-  let
-    dx = (toFloat << abs) (q.x - p.x)
-    dy = (toFloat << abs) (q.y - p.y)
-
-    sx = if p.x < q.x then 1 else -1
-    sy = if p.y < q.y then 1 else -1
-
-    error =
-      (if dx > dy then dx else -dy) / 2
-
-    statics = 
-      BresenhamStatics q sx sy dx dy 
-  in
-  bresenhamLineLoop statics error p []
-    |> List.map ((,) color)
-    |> putMany
-
-
-bresenhamLineLoop : BresenhamStatics -> Float -> Position -> List Position -> List Position
-bresenhamLineLoop statics error p positions =
-  let 
-    positions_ = p :: positions 
-    {sx, sy, dx, dy, finish} = statics
-  in
-  if (p.x == finish.x) && (p.y == finish.y) then 
-    positions_
-  else
     let
-      (dErrX, x) =
-        if error > -dx then (-dy, sx + p.x)
-        else (0, p.x)
+        dx =
+            (toFloat << abs) (q.x - p.x)
 
-      (dErrY, y) =
-        if error < dy then (dx, sy + p.y)
-        else (0, p.y)
+        dy =
+            (toFloat << abs) (q.y - p.y)
 
-      error_ = error + dErrX + dErrY
+        sx =
+            if p.x < q.x then
+                1
+            else
+                -1
+
+        sy =
+            if p.y < q.y then
+                1
+            else
+                -1
+
+        error =
+            if dx > dy then
+                dx / 2
+            else
+                -dy / 2
     in
-      bresenhamLineLoop statics error_ (Position x y) positions_
+        lineLoop (LineStatics q.x q.y sx sy dx dy) error p []
+            |> List.map ((,) color)
+            |> putMany
 
 
+lineLoop : LineStatics -> Float -> Position -> List Position -> List Position
+lineLoop statics error p positions =
+    if (p.x == statics.fx) && (p.y == statics.fy) then
+        p :: positions
+    else
+        let
+            ( error_, q ) =
+                calcError statics error p
+        in
+            lineLoop statics error_ q (p :: positions)
 
 
+calcError : LineStatics -> Float -> Position -> ( Float, Position )
+calcError { sx, sy, dx, dy } error position =
+    let
+        ( errX, x ) =
+            if error > -dx then
+                ( -dy, sx + position.x )
+            else
+                ( 0, position.x )
 
-
+        ( errY, y ) =
+            if error < dy then
+                ( dx, sy + position.y )
+            else
+                ( 0, position.y )
+    in
+        ( error + errX + errY, Position x y )
