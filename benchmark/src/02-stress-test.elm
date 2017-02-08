02495b41c36a5329b2d5523c16fa7c8015f9ec3f
Alex <alex.barry@gmail.com>
2017-02-07 21:32:18 -0500

    Elm format, to the rescue.  I really need to tweak vim to do the 4-space by default.

--- benchmark/src/02-stress-test.elm
+++ benchmark/src/02-stress-test.elm
@@ -8,7 +8,6 @@ import Random
 import Task
 import Date exposing (Date)
 import Time exposing (Time)
-
 import Canvas exposing (Canvas, Position, Size, DrawOp(..))
 
 
@@ -16,195 +15,193 @@ import Canvas exposing (Canvas, Position, Size, DrawOp(..))
 
 
 numberOfRects : Int
-numberOfRects = 500
+numberOfRects =
+    500
 
 
 resolution : Size
-resolution = Size 800 600
+resolution =
+    Size 800 600
 
 
 rectSize : Size
-rectSize = Size 100 100
+rectSize =
+    Size 100 100
+
 
 
 -- Main
 
 
 main =
-  Html.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = always Sub.none
-    }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = always Sub.none
+        }
+
 
 
 -- Types
 
 
 type alias Model =
-  { canvas : Canvas
-  , testStartedAt : Float
-  , rectangles : List Rectangle
-  , results : List TestResult
-  , positionGenerator: Random.Generator Position
-  , colorGenerator: Random.Generator Color
-  }
+    { canvas : Canvas
+    , testStartedAt : Float
+    , rectangles : List Rectangle
+    , results : List TestResult
+    , positionGenerator : Random.Generator Position
+    , colorGenerator : Random.Generator Color
+    }
 
 
 type alias Rectangle =
-  { position : Position
-  , size : Size
-  , color : Color
-  }
+    { position : Position
+    , size : Size
+    , color : Color
+    }
 
 
 type alias TestResult =
-  { index : String
-  , average : Float
-  , totalTime : Float
-  }
+    { index : String
+    , average : Float
+    , totalTime : Float
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-  let
-    canvas : Canvas
-    canvas =
-      Size resolution.width resolution.height
-        |> Canvas.initialize
+    let
+        canvas : Canvas
+        canvas =
+            Size resolution.width resolution.height
+                |> Canvas.initialize
 
-    model : Model
-    model =
-      { canvas = canvas
-      , testStartedAt = 0
-      , rectangles = []
-      , results = []
-      , positionGenerator = Random.map2 Position (Random.int 0 (resolution.width - rectSize.width)) (Random.int 0 (resolution.height - rectSize.height))
-      , colorGenerator = Random.map3 Color.rgb (Random.int 0 255) (Random.int 0 255) (Random.int 0 255)
-      }
-
-  in
-    ( model
-    , Random.generate RandomPosition model.positionGenerator
-    )
+        model : Model
+        model =
+            { canvas = canvas
+            , testStartedAt = 0
+            , rectangles = []
+            , results = []
+            , positionGenerator = Random.map2 Position (Random.int 0 (resolution.width - rectSize.width)) (Random.int 0 (resolution.height - rectSize.height))
+            , colorGenerator = Random.map3 Color.rgb (Random.int 0 255) (Random.int 0 255) (Random.int 0 255)
+            }
+    in
+        ( model
+        , Random.generate RandomPosition model.positionGenerator
+        )
 
 
 defaultRect : Rectangle
 defaultRect =
-  Rectangle
-    (Position 0 0)
-    rectSize
-    (Color.rgb 0 0 0)
+    Rectangle
+        (Position 0 0)
+        rectSize
+        (Color.rgb 0 0 0)
+
+
 
 -- Update
 
 
 type Msg
-  = Benchmark
-  | TestBegin Float
-  | TestEnd Float
-  | RandomPosition Position
-  | RandomColor Position Color
+    = Benchmark
+    | TestBegin Float
+    | TestEnd Float
+    | RandomPosition Position
+    | RandomColor Position Color
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Benchmark ->
-      ( model
-      , Task.perform TestBegin Time.now
-      )
+    case msg of
+        Benchmark ->
+            ( model
+            , Task.perform TestBegin Time.now
+            )
 
+        TestBegin timestamp ->
+            let
+                canvas : Canvas
+                canvas =
+                    Size resolution.width resolution.height
+                        |> Canvas.initialize
+                        |> Canvas.batch drawOps
 
-    TestBegin timestamp ->
-      let
-        canvas : Canvas
-        canvas =
-          Size resolution.width resolution.height
-            |> Canvas.initialize
-            |> Canvas.batch drawOps
+                drawOps : List DrawOp
+                drawOps =
+                    model.rectangles
+                        |> List.concatMap rectOps
 
-        drawOps : List DrawOp
-        drawOps =
-          model.rectangles
-            |> List.concatMap rectOps
+                rectOps : Rectangle -> List DrawOp
+                rectOps rectangle =
+                    [ BeginPath
+                    , FillStyle rectangle.color
+                    , Rect rectangle.position rectangle.size
+                    , Fill
+                    ]
+            in
+                ( { model
+                    | canvas = canvas
+                    , testStartedAt = timestamp
+                  }
+                , Task.perform TestEnd Time.now
+                )
 
+        TestEnd timestamp ->
+            let
+                result : TestResult
+                result =
+                    let
+                        delta : Float
+                        delta =
+                            timestamp - model.testStartedAt
 
-        rectOps : Rectangle -> List DrawOp
-        rectOps rectangle =
-          [ BeginPath
-          , FillStyle rectangle.color
-          , Rect rectangle.position rectangle.size
-          , Fill
-          ]
+                        average : Float
+                        average =
+                            delta / (toFloat (List.length model.rectangles))
+                    in
+                        { index = toString ((List.length model.results) + 1)
+                        , average = average
+                        , totalTime = delta
+                        }
 
-      in
-        ( { model |
-            canvas = canvas
-          , testStartedAt = timestamp
-          }
-        , Task.perform TestEnd Time.now
-        )
+                results : List TestResult
+                results =
+                    List.append model.results [ result ]
+            in
+                ( { model | results = results }
+                , Cmd.none
+                )
 
+        RandomPosition position ->
+            if (List.length model.rectangles) < numberOfRects then
+                ( model
+                , Random.generate (RandomColor position) model.colorGenerator
+                )
+            else
+                ( model
+                , Cmd.none
+                )
 
-    TestEnd timestamp ->
-      let
-        result : TestResult
-        result =
-          let
-            delta : Float
-            delta = timestamp - model.testStartedAt
+        RandomColor position color ->
+            let
+                rectangle : Rectangle
+                rectangle =
+                    Rectangle
+                        position
+                        rectSize
+                        color
 
-            average : Float
-            average =
-              delta / (toFloat (List.length model.rectangles))
+                rectangles : List Rectangle
+                rectangles =
+                    List.append model.rectangles [ rectangle ]
+            in
+                ( { model | rectangles = rectangles }
+                , Random.generate RandomPosition model.positionGenerator
+                )
 
-          in
-            { index = toString ((List.length model.results) + 1)
-            , average = average
-            , totalTime = delta
-            }
-
-        results : List TestResult
-        results =
-          List.append model.results [result]
-
-      in
-        ( { model | results = results }
-        , Cmd.none
-        )
-
-
-    RandomPosition position ->
-      if (List.length model.rectangles) < numberOfRects then
-        ( model
-        , Random.generate (RandomColor position) model.colorGenerator
-        )
-
-      else
-        ( model
-        , Cmd.none
-        )
-
-
-    RandomColor position color ->
-      let
-        rectangle : Rectangle
-        rectangle =
-          Rectangle
-            position
-            rectSize
-            color
-
-        rectangles : List Rectangle
-        rectangles =
-          List.append model.rectangles [rectangle]
-
-      in
-        ( { model | rectangles = rectangles }
-        , Random.generate RandomPosition model.positionGenerator
-        )
 
 
 -- View
@@ -212,111 +209,110 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  let
-    resultsHtml : List TestResult -> Html Msg
-    resultsHtml results =
-      let
-        msToString : Float -> String
-        msToString milliseconds =
-          (toString milliseconds) ++ "ms"
+    let
+        resultsHtml : List TestResult -> Html Msg
+        resultsHtml results =
+            let
+                msToString : Float -> String
+                msToString milliseconds =
+                    (toString milliseconds) ++ "ms"
 
-        tdStyles : List (String, String)
-        tdStyles =
-          [ ( "border-top", "1px black solid" )
-          , ( "text-align", "center" )
-          ]
+                tdStyles : List ( String, String )
+                tdStyles =
+                    [ ( "border-top", "1px black solid" )
+                    , ( "text-align", "center" )
+                    ]
 
-        tdAttributes : List (Html.Attribute Msg)
-        tdAttributes =
-          [ style tdStyles
-          ]
+                tdAttributes : List (Html.Attribute Msg)
+                tdAttributes =
+                    [ style tdStyles
+                    ]
 
-        renderRow : TestResult -> Html Msg
-        renderRow result =
-          tr
+                renderRow : TestResult -> Html Msg
+                renderRow result =
+                    tr
+                        []
+                        [ td
+                            [ style [ ( "border-top", "1px black solid" ) ] ]
+                            [ text (result.index ++ ".") ]
+                        , td tdAttributes [ text (msToString result.average) ]
+                        , td tdAttributes [ text (msToString result.totalTime) ]
+                        ]
+
+                thStyle : List ( String, String )
+                thStyle =
+                    [ ( "width", "100px" )
+                    ]
+            in
+                if (List.length results) > 0 then
+                    table
+                        [ style
+                            [ ( "border-collapse", "collapse" )
+                            ]
+                        ]
+                        [ thead
+                            []
+                            [ tr
+                                []
+                                [ th [ style thStyle ] [ text "" ]
+                                , th [ style thStyle ] [ text "Average" ]
+                                , th [ style thStyle ] [ text "Total time" ]
+                                ]
+                            ]
+                        , tbody
+                            []
+                            (List.map renderRow results)
+                        ]
+                else
+                    text ""
+
+        averageResult : TestResult
+        averageResult =
+            { index = "Average"
+            , average = (\sum -> sum / (toFloat <| List.length model.results)) <| List.sum <| List.map .average model.results
+            , totalTime = (\sum -> sum / (toFloat <| List.length model.results)) <| List.sum <| List.map .totalTime model.results
+            }
+
+        allResults : List TestResult
+        allResults =
+            if (List.length model.results) > 1 then
+                List.append model.results [ averageResult ]
+            else
+                model.results
+
+        buttonDisabled : Bool
+        buttonDisabled =
+            (List.length model.rectangles) < numberOfRects
+
+        buttonText : String
+        buttonText =
+            if buttonDisabled then
+                "Preloading rectangle data..."
+            else
+                "Begin benchmark"
+    in
+        div
             []
-            [ td
-              [ style [( "border-top", "1px black solid" )] ]
-              [ text (result.index ++ ".") ]
-            , td tdAttributes [ text (msToString result.average) ]
-            , td tdAttributes [ text (msToString result.totalTime) ]
-            ]
-
-        thStyle : List ( String, String )
-        thStyle =
-          [ ("width", "100px")
-          ]
-
-      in
-        if (List.length results) > 0 then
-          table
-            [ style
-              [ ("border-collapse", "collapse")
-              ]
-            ]
-            [ thead
-              []
-              [ tr
+            [ div
                 []
-                [ th [ style thStyle ] [ text "" ]
-                , th [ style thStyle ] [ text "Average" ]
-                , th [ style thStyle ] [ text "Total time" ]
+                [ text
+                    ("Time to batch render "
+                        ++ (toString numberOfRects)
+                        ++ " opaque rectangles on a "
+                        ++ (toString resolution.width)
+                        ++ "x"
+                        ++ (toString resolution.height)
+                        ++ " canvas"
+                    )
                 ]
-              ]
-            , tbody
-              []
-              (List.map renderRow results)
+            , div
+                []
+                [ button
+                    [ onClick Benchmark
+                    , disabled buttonDisabled
+                    ]
+                    [ text buttonText ]
+                ]
+            , Canvas.toHtml [] model.canvas
+            , resultsHtml allResults
             ]
-
-        else
-          text ""
-
-    averageResult : TestResult
-    averageResult =
-      { index = "Average"
-      , average = (\sum -> sum / (toFloat <| List.length model.results)) <| List.sum <| List.map .average model.results
-      , totalTime = (\sum -> sum / (toFloat <| List.length model.results)) <| List.sum <| List.map .totalTime model.results
-      }
-
-    allResults : List TestResult
-    allResults =
-      if (List.length model.results) > 1 then
-        List.append model.results [averageResult]
-      else
-        model.results
-
-    buttonDisabled : Bool
-    buttonDisabled =
-      (List.length model.rectangles) < numberOfRects
-
-    buttonText : String
-    buttonText =
-      if buttonDisabled then
-        "Preloading rectangle data..."
-
-      else
-        "Begin benchmark"
-
-  in
-    div
-      []
-      [ div
-        []
-        [ text
-          ( "Time to batch render "
-          ++ (toString numberOfRects)
-          ++ " opaque rectangles on a "
-          ++ (toString resolution.width) ++ "x" ++ (toString resolution.height)
-          ++ " canvas" )
-        ]
-      , div
-        []
-        [ button
-          [ onClick Benchmark
-          , disabled buttonDisabled
-          ]
-          [ text buttonText ]
-        ]
-      , Canvas.toHtml [] model.canvas
-      , resultsHtml allResults
-      ]
